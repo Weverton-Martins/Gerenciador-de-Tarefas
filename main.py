409c27bcde19866fc39081e7a31963a0154e5709
Weverton Martins <wevertonwhm18@outlook.com>
2026-05-25 17:09:05 -0300

    finalizado

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#Este aplicativo permitirá que você:
 '''
-Adicione novas tarefas com título, descrição e data de conclusão.
+Este aplicativo permitirá que você:
+● Adicione novas tarefas com título, descrição e data de conclusão.
 ● Listar todas as tarefas, categorizando-as como pendentes ou concluídas.
 ● Marcar tarefas como concluídas.
 ● Remover tarefas.
@@ -13,16 +13,28 @@ import json
 import os
 from datetime import datetime
 
+def carregar_tarefas():
+    if os.path.exists('tarefas.json'):
+        with open('tarefas.json', 'r', encoding='utf-8') as arquivo: #puxa o arquivo em modo de leitura 'r'
+        #lê o que esta no arquivo e guarda em uma variavel
+            return json.load(arquivo)
+    else:
+        return []
+    
+def salvar_tarefas():
+    with open('tarefas.json', 'w', encoding='utf-8') as arquivo: #cria e abre o arquivo chamado 'tarefa.json' em modo de escrita 'w'
+        json.dump(lista_tarefas, arquivo, indent= 4) #despeja minha lista no arquivo
+    print('Tarefas salvas com sucesso no disco!')
+
 # Estrutura lógica de uma tarefa, como serão os dicionarios armazenados
 '''
 tarefa = {
-    "id": 1,
+    "id": 1, #Vai ser taribuido de forma automatica
     "titulo": "Estudar Python",
     "descricao": "Focar em funções e dicionários",
     "data_conclusao": "25/05/2026",
     "concluida": False  # Toda tarefa nova nasce como False (pendente)
 }'''
-
 lista_tarefas = []
 
 #Primeiro adicionar tarefa
@@ -30,8 +42,7 @@ def adicionar_tarefa():
     print('Adicione a tarefa desejada:')
     tarefa = input('Tarefa: ')
     desc = input('Descrição: ')
-    data = input('Data de Conclusão (formato DD/MM/YYYY): ')
-    data_formatada = datetime.strptime(data, "%d/%m/%Y")#Formatar a data
+    data = input('Data de Conclusão (formato DD/MM/YYYY): ')#passada como str mas alterada quando ordenado por data
     id = len(lista_tarefas) +1
     
     #Criando o dicionario com os valores solciitados
@@ -39,21 +50,20 @@ def adicionar_tarefa():
         'id': id,
         'tarefa': tarefa,
         'descrição': desc,
-        'data_conclusão': data_formatada,
+        'data_conclusão': data,
         'concluida': False
     }
     #adicionando o dicionario na lista
     lista_tarefas.append(dicionario_tarefa)
 
 def listar_tarefas():
-    print('Lista de tarefas:')
+    print('\nLista de tarefas:')
     for lista in lista_tarefas:
-        data_bonita = lista['data_conclusão'].strftime("%d/%m/%Y")
 
         print(f'id: {lista["id"]} | status: {'Concluida' if lista.get("concluida") else "Pendente"}')
         print(f'Tarefa: {lista["tarefa"]}')
         print(f'Descrição: {lista["descrição"]}')
-        print(f'Data: {data_bonita}')
+        print(f'Data: {lista['data_conclusão']}')
         print("-" * 30)
 
 def marcar_concluida():
@@ -63,22 +73,53 @@ def marcar_concluida():
     tarefa_encontrada = False #marcação pra saber se foi achado o ID
 
     for lista in lista_tarefas:
+        #modificando o ID pra concluido se for encontrado na lista
         if lista["id"] == identificador:
             lista["concluida"] = True
             print('Status modificado com sucesso')
             tarefa_encontrada = True
             break
 
-        if not tarefa_encontrada:
-            print('Nenhuma tarefa com esse ID enconmtrada!')
+    if not tarefa_encontrada:
+        print('Nenhuma tarefa com esse ID enconmtrada!')
 
 def remover_tarefa():
     print('Removendo Tarefas')
     remove = input('Informe qual tarefa deseja remover: ').lower()
     for lista in lista_tarefas:
+        #se lista encontrada a remove
         if lista['tarefa'] == remove:
             lista_tarefas.remove(lista)
             print('Removida com sucesso!')
+
+def pesquisar_tarefa():
+    print('Pesquise tarefas por titulo ou descrição:')
+    title = input('Informe qual o titulo ou descrição que deseja procurar: ').lower() #termo a ser pesquisado
+
+    tarefa_encontrada = False #condição se deu certo ou não
+    for lista in lista_tarefas:
+        #mostra ao usuario apenas a lista desejada
+        if (title in lista['tarefa']) or (title in lista['descrição']):
+           
+            print(f'id: {lista["id"]} | status: {'Concluida' if lista.get("concluida") else "Pendente"}')
+            print(f'Tarefa: {lista["tarefa"]}')
+            print(f'Descrição: {lista["descrição"]}')
+            print(f'Data: {lista['data_conclusão']}')
+            print("-" * 30)
+
+            tarefa_encontrada = True
+    if not tarefa_encontrada: #caso não encontre o termo continua como false e retorna a mensagem 
+        print('Nenhuma tarefa encontrada com esse termo!')
+
+def extrair_datas(tarefa): #função pra 'traduzir' a data pro python, pois esta lendo como str como um texto
+    texto_data = tarefa['data_conclusão']
+    return datetime.strptime(texto_data, "%d/%m/%Y") #aqui pra saber qual data vem antes e não em alfabetica
+
+def ordenar_pordata():
+    print('\n---Ordenando Por Datas---\n')
+    
+    lista_tarefas.sort(key=extrair_datas) #o key diz pra usarmos nossa função pra pegar a ordem correta
+    print('Tarefas ordenadas com sucesso!')
 
 while True:
     print('\n____Menu____')
@@ -86,7 +127,9 @@ while True:
     print('2 - Listar')
     print('3 - Marcar como Concluida')
     print('4 - Remover')
-    print('5 - Sair')
+    print('5 - Pesquisar')
+    print('6 - Ordenar Por Data')
+    print('7 - Salvar e Sair')
 
     opcao = int(input('\nEscolha uma opção: '))
 
@@ -99,4 +142,11 @@ while True:
     elif opcao == 4:
         remover_tarefa()
     elif opcao == 5:
+        pesquisar_tarefa()
+    elif opcao == 6:
+        ordenar_pordata()
+    elif opcao == 7:
+        salvar_tarefas()
         break
+    else:
+        print('Opção invalida!')
