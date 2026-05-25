43a0a69e2497fd4622cebfbc284e3b7211f9aff1
Weverton Martins <wevertonwhm18@outlook.com>
2026-05-25 15:17:27 -0300

    .

--- main.py
+++ main.py
@@ -37,26 +37,66 @@ def adicionar_tarefa():
     #Criando o dicionario com os valores solciitados
     dicionario_tarefa = {
         'id': id,
-        'Tarefa': tarefa,
-        'Descrição': desc,
+        'tarefa': tarefa,
+        'descrição': desc,
         'data_conclusão': data_formatada,
         'concluida': False
     }
     #adicionando o dicionario na lista
     lista_tarefas.append(dicionario_tarefa)
-#fazer ainda função listar,remover,status etc
+
+def listar_tarefas():
+    print('Lista de tarefas:')
+    for lista in lista_tarefas:
+        data_bonita = lista['data_conclusão'].strftime("%d/%m/%Y")
+
+        print(f'id: {lista["id"]} | status: {'Concluida' if lista.get("concluida") else "Pendente"}')
+        print(f'Tarefa: {lista["tarefa"]}')
+        print(f'Descrição: {lista["descrição"]}')
+        print(f'Data: {data_bonita}')
+        print("-" * 30)
+
+def marcar_concluida():
+    print('Organizando as concluidas e Pendentes:')
+    identificador = int(input('Informe o ID que deseja modificar: '))
+
+    tarefa_encontrada = False #marcação pra saber se foi achado o ID
+
+    for lista in lista_tarefas:
+        if lista["id"] == identificador:
+            lista["concluida"] = True
+            print('Status modificado com sucesso')
+            tarefa_encontrada = True
+            break
+
+        if not tarefa_encontrada:
+            print('Nenhuma tarefa com esse ID enconmtrada!')
+
+def remover_tarefa():
+    print('Removendo Tarefas')
+    remove = input('Informe qual tarefa deseja remover: ').lower()
+    for lista in lista_tarefas:
+        if lista['tarefa'] == remove:
+            lista_tarefas.remove(lista)
+            print('Removida com sucesso!')
 
 while True:
     print('\n____Menu____')
     print('1 - Adicionar Tarefa')
     print('2 - Listar')
-    print('3 - Sair')
+    print('3 - Marcar como Concluida')
+    print('4 - Remover')
+    print('5 - Sair')
 
-    opcao = int(input('Escolha uma opção: \n'))
+    opcao = int(input('\nEscolha uma opção: '))
 
     if opcao == 1:
         adicionar_tarefa()
     elif opcao == 2:
-        print(lista_tarefas)
+        listar_tarefas()
     elif opcao == 3:
+        marcar_concluida()
+    elif opcao == 4:
+        remover_tarefa()
+    elif opcao == 5:
         break
